017f43f11cc14dafbb4afe48ef004dac2e4a3d89
piyush31508 <chawlapiyush811@gmail.com>
2026-06-25 15:22:54 +0530

    Add staging workflow

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,26 @@
 module.exports = {
   apps: [
     {
-      name: 'tcbt-app-server',
-      script: './dist/main.js',
+      name: "tcbt-app-server",
+
+      cwd: "/home/ubuntu/app/current",
+
+      script: "dist/main.js",
+
       instances: 1,
-      exec_mode: 'fork',
+
+      exec_mode: "fork",
+
       watch: false,
+
+      autorestart: true,
+
+      max_restarts: 5,
+
+      restart_delay: 3000,
+
       env: {
-        NODE_ENV: 'production',
+        NODE_ENV: "production",
         PORT: 3000,
       },
     },
